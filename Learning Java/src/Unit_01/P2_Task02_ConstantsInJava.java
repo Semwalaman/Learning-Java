03ab8a88a7ddb7174d915bb15da27c32bf489bdd
Other <Other@LAPTOP-IMM7EBK5>
2022-03-30 13:04:32 +0530

    commit

---
+++ Learning Java/src/Unit_01/P2_Task02_ConstantsInJava.java
@@ -0,0 +1,11 @@
+package Unit_01;
+
+public class P2_Task02_ConstantsInJava {
+    public static final double pi=3.14;
+    public static void main(String[] args)
+    {
+    	final int a=20;
+    	System.out.println(pi);
+    	System.out.println(a);
+    }
+}
