90a27e0ea2d00af1de2cd0c5c895304bb7c8a307
Other <Other@164.123.54.64>
2022-03-31 22:34:05 +0530

    commit

--- Learning Java/src/Unit_01/P3_Task02_OperatorsInJava.java
+++ Learning Java/src/Unit_01/P3_Task02_OperatorsInJava.java
@@ -1,74 +1,81 @@
 package Unit_01;
-
 public class P3_Task02_OperatorsInJava {
     public static void main(String[] args) {
-        P3_Task02_OperatorsInJava obj = new P3_Task02_OperatorsInJava();
-        obj.UnaryOperators(); //a++,++a,a--,--a,~a,!a
-        obj.ArithmeticOperators();  // *,/,-,+,%
-        obj.ShiftOperators(); //<<,>>
-        obj.RelationalOperators();  // <, >, <=, >=, ==, !=
-        obj.BitWiseOperatorsAndLogical(); // &, ^, | 
-    }
+        /*
+         * OPERATORS IN JAVA
+         * Unary Operator,
+         * Arithmetic Operator,
+         * Shift Operator,
+         * Relational Operator,
+         * Bitwise Operator,
+         * Logical Operator,
+         * Ternary Operator and
+         * Assignment Operator.
+         */
 
-    void UnaryOperators()
-    {
-        int a=5;
-        boolean b=true;
-        System.out.println("Unary Operators");
-        System.out.println(a++);
-        System.out.println(a--);
-        System.out.println(++a);
-        System.out.println(--a);
-        System.out.println(~a);
-        System.out.println(!b);
-        System.out.println();
-    }
+         //Unary Operators
+        int x = 10;
+        System.out.println(x++);// 10 (11)
+        System.out.println(++x);// 12
+        System.out.println(x--);// 12 (11)
+        System.out.println(--x);// 10
 
-    void ArithmeticOperators()
-    {
-        System.out.println("Arithmetic Operators");
-        int a=10;
-        int b=30;
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a*b);
-        System.out.println(b/a);
-        System.out.println(a%b);
-        System.out.println(a%b/5+4-3*3);
-        System.out.println();
-    }
+        //Arithmetic Operators
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);// 30
+        System.out.println(a - b);// -10
+        System.out.println(a * b);// 200
+        System.out.println(a / b);// 0
+        System.out.println(a % b);// 10
 
-    void ShiftOperators()
-    {
-        System.out.println("Shift Operators");
-        System.out.println(10<<2); //10*2^2=40
-        System.out.println(10<<1); //10*2^1=20
-        System.out.println(10>>2); //10/2^2=2
-        System.out.println(20>>4); //20/2^4=1
-        System.out.println();
-    }
+        //Shift Operators
+        int c = 10;
+        int d = 20;
+        System.out.println(c << d);// 100
+        System.out.println(c >> d);// 0
+        System.out.println(c >>> d);// 0
 
-    void RelationalOperators(){
-        System.out.println("Relational Operators");
-        int a=3,b=3,c=5;
-        System.out.println(a==b);
-        System.out.println(a!=b);
-        System.out.println(a>c);
-        System.out.println(a<=c);
-        System.out.println();
-    }
+        //Relational Operators
+        int e = 10;
+        int f = 20;
+        System.out.println(e == f);// false
+        System.out.println(e != f);// true
+        System.out.println(e > f);// false
+        System.out.println(e < f);// true
+        System.out.println(e >= f);// false
+        System.out.println(e <= f);// true
 
-    void BitWiseOperatorsAndLogical()
-    {
-        int a=5,b=10,c=20;
-        System.out.println("Bitwise and Logical Operators");
-        System.out.println(a<b && b++<c);
-        System.out.println(a++>b || b>c);
-        System.out.println(b);
-        System.out.println(a);
-        System.out.println(a>b | a<c);
-        System.out.println(a<b & a++<c);
-        System.out.println(a);
-    }
+        //Bitwise Operators
+        int g = 10;
+        int h = 20;
+        System.out.println(g & h);// 0
+        System.out.println(g | h);// 30
+        System.out.println(g ^ h);// 30
+        System.out.println(~g);// -11
+        System.out.println(g << 2);// 40
+        System.out.println(g >> 2);// 2
 
+        //Logical Operators
+        boolean i = true;
+        boolean j = false;
+        System.out.println(i && j);// false
+        System.out.println(i || j);// true
+        System.out.println(!i);// false
+
+        //Ternary Operator
+        int k = 10;
+        int l = 20;
+        int m = k > l ? k : l;
+        System.out.println(m);// 20
+
+        //Assignment Operators
+        int n = 10;
+        int o = 20;
+        n += o;
+        System.out.println(n);// 30
+        n -= o;
+        System.out.println(n);// 10
+
+    }
 }
